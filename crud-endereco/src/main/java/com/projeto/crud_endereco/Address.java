61e9a17459017fa9db8863f5d4e52c5b037bb681
Caio Cardoso <cardosoterracaio@gmail.com>
2025-03-02 15:22:29 -0300

    Projeto para faculdade
    Signed-off-by: Caio Cardoso <cardosoterracaio@gmail.com>

---
+++ crud-endereco/src/main/java/com/projeto/crud_endereco/Address.java
@@ -0,0 +1,38 @@
+package com.projeto.crud_endereco;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Address() {}
+
+    public Address(String street, String city, String state, String zipCode) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public Long getId() { return id; }
+    public String getStreet() { return street; }
+    public String getCity() { return city; }
+    public String getState() { return state; }
+    public String getZipCode() { return zipCode; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setStreet(String street) { this.street = street; }
+    public void setCity(String city) { this.city = city; }
+    public void setState(String state) { this.state = state; }
+    public void setZipCode(String zipCode) { this.zipCode = zipCode; }
+}
---
+++ crud-endereco/src/main/java/com/projeto/crud_endereco/CrudEnderecoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.crud_endereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEnderecoApplication.class, args);
+	}
+
+}
